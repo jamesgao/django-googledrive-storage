d6ba30ab3a1b298f33be5f2b7b6f788d41c28abd
James Gao <jamesgao@gmail.com>
2017-02-01 21:42:27 -0800

    lazy service object

--- gdstorage/storage.py
+++ gdstorage/storage.py
@@ -7,6 +7,7 @@ import logging
 import random
 import time
 import random
+import warnings
 
 import django
 import enum
@@ -225,31 +226,35 @@ class GoogleDriveStorage(Storage):
         :raise ValueError:
         """
         self._json_keyfile_path = json_keyfile_path or settings.GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE
-
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(self._json_keyfile_path,
-                                                                       scopes=["https://www.googleapis.com/auth/drive"])
-        if delegate is not None:
-            credentials = credentials.create_delegated(delegate)
-
-        http = httplib2.Http()
-        http = credentials.authorize(http)
-
+        self._drive_service = None
         self._permissions = None
         if permissions is None:
             self._permissions = (_ANYONE_CAN_READ_PERMISSION_,)
         else:
             self.update_permissions(permissions)
+        self._delegate = delegate
 
-        retries = 0
-        self._drive_service = None
-        while self._drive_service is None:
-            try:
-                self._drive_service = build('drive', 'v3', http=http)
-            except HttpAccessTokenRefreshError:
-                time.sleep(2**retries+random.random())
-                retries += 1
-                if retries > 20:
-                    raise RuntimeError("Exceeded retry limit")
+    @property
+    def service(self):
+        if self._drive_service is None:
+            retries = 0
+            self._drive_service = None
+            while self._drive_service is None:
+                try:
+                    creds = ServiceAccountCredentials.from_json_keyfile_name(
+                        self._json_keyfile_path,
+                        scopes=["https://www.googleapis.com/auth/drive"])
+                    if self._delegate is not None:
+                        creds = creds.create_delegated(self._delegate)
+                    http = creds.authorize(httplib2.Http())
+                    self._drive_service = build('drive', 'v3', http=http)
+                except HttpAccessTokenRefreshError as e:
+                    warnings.warn("Error refreshing token: %s"%e)
+                    time.sleep(2**retries+random.random())
+                    retries += 1
+                    if retries > 5:
+                        raise RuntimeError("Exceeded retry limit")
+        return self._drive_service
 
     def _split_path(self, p):
         """
@@ -277,6 +282,7 @@ class GoogleDriveStorage(Storage):
         """
         folder_data = self._find_file(path, parent_id)
         if folder_data is None:
+            import ipdb; ipdb.set_trace()
             # Folder does not exists, have to create
             split_path = self._split_path(path)
             current_folder_data = None
@@ -292,7 +298,7 @@ class GoogleDriveStorage(Storage):
                     # obtained by the user, if available
                     if parent_id is not None:
                         meta_data['parents'] = [parent_id]
-                current_folder_data = self._drive_service.files().create(body=meta_data).execute()
+                current_folder_data = self.service.files().create(body=meta_data).execute()
             return current_folder_data
         else:
             return folder_data
@@ -320,7 +326,7 @@ class GoogleDriveStorage(Storage):
             if parent_id is not None:
                 q = "{0} and '{1}' in parents".format(q, parent_id)
             max_results = 1000  # Max value admitted from google drive
-            folders = self._drive_service.files().list(q=q, pageSize=max_results).execute()
+            folders = self.service.files().list(q=q, pageSize=max_results).execute()
             for folder in folders["files"]:
                 if folder["name"] == split_filename[0]:
                     # Assuming every folder has a single parent
@@ -332,10 +338,10 @@ class GoogleDriveStorage(Storage):
             if parent_id is not None:
                 q = "{0} and '{1}' in parents".format(q, parent_id)
             max_results = 1000  # Max value admitted from google drive
-            file_list = self._drive_service.files().list(q=q, pageSize=max_results).execute()
+            file_list = self.service.files().list(q=q, pageSize=max_results).execute()
             if len(file_list["files"]) == 0:
                 q = "" if parent_id is None else "'{0}' in parents".format(parent_id)
-                file_list = self._drive_service.files().list(q=q, pageSize=max_results).execute()
+                file_list = self.service.files().list(q=q, pageSize=max_results).execute()
                 for element in file_list["files"]:
                     if split_filename[0] in element["name"]:
                         return element
@@ -344,7 +350,7 @@ class GoogleDriveStorage(Storage):
                 return file_list["files"][0]
 
     def _get(self, fileId, **kwargs):
-        return self._drive_service.files().get(fileId=fileId, **kwargs).execute()
+        return self.service.files().get(fileId=fileId, **kwargs).execute()
 
     def update_permissions(self, permissions):
         if isinstance(permissions, (tuple, list,)):
@@ -362,7 +368,7 @@ class GoogleDriveStorage(Storage):
     # to create a valid storage for Django
 
     def _open(self, name, mode='rb'):
-        request = self._drive_service.files().get_media(fileId=name)
+        request = self.service.files().get_media(fileId=name)
         return ChunkFile(request, self._get(name))
 
     def _save(self, name, content):
@@ -390,18 +396,18 @@ class GoogleDriveStorage(Storage):
         if fileId is not None:
             body['id'] = fileId
 
-        file_data = self._drive_service.files().create(
+        file_data = self.service.files().create(
             body=body,
             media_body=media_body).execute(num_retries=20)
 
         # Setting up permissions
         for p in self._permissions:
-            self._drive_service.permissions().create(fileId=file_data["id"], body=p, sendNotificationEmail=False).execute()
+            self.service.permissions().create(fileId=file_data["id"], body=p, sendNotificationEmail=False).execute()
 
         return file_data['id']
 
     def get_available_name(self, name, max_length=None):
-        response = self._drive_service.files().generateIds(count=1).execute()
+        response = self.service.files().generateIds(count=1).execute()
         fileId = response['ids'][0]
         return name, fileId
 
@@ -409,7 +415,7 @@ class GoogleDriveStorage(Storage):
         """
         Deletes the specified file from the storage system.
         """
-        self._drive_service.files().delete(fileId=name).execute()
+        self.service.files().delete(fileId=name).execute()
 
     def exists(self, name):
         """
@@ -437,8 +443,8 @@ class GoogleDriveStorage(Storage):
                 'q': "'{0}' in parents and mimeType = '{1}'".format(folder_id["id"],
                                                                     self._GOOGLE_DRIVE_FOLDER_MIMETYPE_),
             }
-            files_list = self._drive_service.files().list(**file_params).execute()
-            dir_list = self._drive_service.files().list(**dir_params).execute()
+            files_list = self.service.files().list(**file_params).execute()
+            dir_list = self.service.files().list(**dir_params).execute()
             for element in files_list["files"]:
                 files.append(os.path.join(path, element["name"]))
             for element in dir_list["files"]:
